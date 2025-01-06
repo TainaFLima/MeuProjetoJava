f2d831b072192cecbfd7f28ef417a6485cc3638a
Taina Ferreira <taina.sofie@gmail.com>
2025-01-06 17:23:32 -0300

    Adicionando exercício de concatenação

--- src/CastingExemplo.java
+++ src/CastingExemplo.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class CastingExemplo {
+    public static void main(String[] args) {
+
+        double numero1 = 28.5;
+        int numeroInteiro = (int) numero1;
+
+        System.out.println("Valor original (double): " + numero1);
+        System.out.println("Valor convertido (int): " + numeroInteiro);
+
+    }
 }
---
+++ src/Concatenar.java
@@ -0,0 +1,9 @@
+public class Concatenar {
+    public static void main(String[] args) {
+
+        char inicial = 'D';
+        String nome = "Vader";
+
+        System.out.println("o nome é: " + inicial + nome);
+    }
+}
