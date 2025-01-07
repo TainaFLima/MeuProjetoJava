b9a457af3709552779cb29de69b68a88999f4117
Taina Ferreira <taina.sofie@gmail.com>
2025-01-07 07:59:05 -0300

    Adicionando exercício do cálculo do total de um produto

---
+++ src/TotalProduto.java
@@ -0,0 +1,9 @@
+public class TotalProduto {
+    public static void main(String[] args) {
+        double precoProduto = 5.10;
+                int quantidade = 10;
+                        double resultado = precoProduto * quantidade;
+                            System.out.println("O valor total é: R$" + resultado);
+
+    }
+}
